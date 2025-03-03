051ca69b4101dca18f66ce61cbfff91b72ccac57
An <quyan2004@gmail.com>
2025-03-04 00:04:41 +0700

    aa

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Đảm bảo đường dẫn tương đối
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
